0ed9d2dbb2d9a671a6212d0b3c9041fdc4be0928
Photonicgeek <nathankalish@me.com>
2014-12-15 01:14:40 -0800

    bug fix for cloning

--- nethunterbuild.sh
+++ nethunterbuild.sh
@@ -83,6 +83,7 @@ nhb_setup(){
   else
     echo "NetHunter build directory not found. Installing..."
     git clone -b nethunterbuild https://github.com/offensive-security/kali-nethunter $maindir
+    mkdir -p $maindir/rootfs
     ### Make Directories and Prepare to build
     git clone https://github.com/offensive-security/gcc-arm-linux-gnueabihf-4.7 $toolchaindir/gcc-arm-linux-gnueabihf-4.7
     export PATH=${PATH}:$toolchaindir/gcc-arm-linux-gnueabihf-4.7/bin
@@ -91,10 +92,16 @@ nhb_setup(){
     apt-get install -y git-core gnupg flex bison gperf libesd0-dev build-essential zip curl libncurses5-dev zlib1g-dev libncurses5-dev gcc-multilib g++-multilib \
     parted kpartx debootstrap pixz qemu-user-static abootimg cgpt vboot-kernel-utils vboot-utils uboot-mkimage bc lzma lzop automake autoconf m4 dosfstools pixz rsync \
     schedtool git dosfstools e2fsprogs device-tree-compiler ccache dos2unix
-    dpkg --add-architecture i386
-    apt-get install -y ia32-libs
-    ### Required for kernel cross compiles
-    apt-get install -y libncurses5:i386
+    MACHINE_TYPE=`uname -m`
+    if [ ${MACHINE_TYPE} == 'x86_64' ]; then
+      dpkg --add-architecture i386
+      apt-get update
+      apt-get install -y ia32-libs
+      # Required for kernel cross compiles
+      apt-get install -y libncurses5:i386
+    else
+      apt-get install -y libncurses5
+    fi
     if [ ! -e "/usr/bin/lz4c" ]; then
       echo "Missing lz4c which is needed to build certain kernels.  Downloading and making for system:"
       cd $maindir
@@ -107,7 +114,6 @@ nhb_setup(){
       cd ..
       rm -rf lz4-r112.tar.gz lz4-r112
     fi
-    mkdir -p $maindir/rootfs
     cd $maindir
   fi
 
