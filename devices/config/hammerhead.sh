512d4b82ff4bbe79c9dba75e746350bdcb443844
Photonicgeek <nathankalish@me.com>
2014-12-19 11:35:43 -0800

    fixed another typo

--- devices/config/hammerhead.sh
+++ devices/config/hammerhead.sh
@@ -40,7 +40,9 @@ nhb_hammerhead_lollipop(){
 		make kali_defconfig
 		# Attach kernel builder to updater-script
 		cp $maindir/devices/updater-scripts/lollipop/hammerhead $workingdir/flashkernel/META-INF/com/google/android/updater-script
-		f_kernel_build
+
+		nhb_kernel_build
+
 		cd $workingdir/flashkernel/kernel
 		abootimg --create $workingdir/flashkernel/boot.img -f $workingdir/kernel/ramdisk/5/bootimg.cfg -k $workingdir/kernel/arch/arm/boot/zImage-dtb -r $workingdir/kernel/ramdisk/5/initrd.img
 		cd $workingdir
