d8dc2af822021ae494d0aa0748c318b7d681d9db
Simon Brannstrom <unknownpwn@users.noreply.github.com>
2015-01-10 03:37:54 +0100

    Update hid-all.py
    Added options for Belgian and Norwegian.

--- utils/hid/hid-all.py
+++ utils/hid/hid-all.py
@@ -11,6 +11,8 @@ parser.add_argument('--es', help='Select ES keyboard mapping', action='store_tru
 parser.add_argument('--de', help='Select DE keyboard mapping', action='store_true')
 parser.add_argument('--sv', help='Select SV keyboard mapping', action='store_true')
 parser.add_argument('--dk', help='Select DK keyboard mapping', action='store_true')
+parser.add_argument('--be', help='Select BE keyboard mapping', action='store_true')
+parser.add_argument('--no', help='Select NO keyboard mapping', action='store_true')
 parser.add_argument('--wincmd', '-w', help='Windows CMD', action='store_true')
 parser.add_argument('--win7cmd', '-w7', help='Windows 7 CMD elevated', action='store_true')
 parser.add_argument('--win8cmd','-w8', help='Windows 8 CMD elevated', action='store_true')
@@ -34,6 +36,10 @@ elif (args.sv):
 	locale='sv'
 elif (args.dk):
         locale='dk'
+elif (args.be):
+	locale='be'
+elif (args.no):
+	locale='no'
 
 def read_file(filename):
 	try:
